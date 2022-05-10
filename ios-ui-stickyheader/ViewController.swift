f18cffc346c42b29a603e6863211605f63d56ba9
jmkim <wjdals0304@gmail.com>
2022-05-10 14:41:42 +0900

    Initial Commit

---
+++ ios-ui-stickyheader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-ui-stickyheader
+//
+//  Created by 김정민 on 2022/05/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
